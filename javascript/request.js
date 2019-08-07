a526beed5d3d662c90263079cba2256620ad42de
honglikehk <christine.hong.to@gmail.com>
2019-08-07 12:12:10 -0400

    cleaned up code

--- javascript/request.js
+++ javascript/request.js
@@ -15,18 +15,8 @@ $(document).ready(function() {
     "Excited",
     "Stressed"
   ];
-  /*
-  let queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=4nfHq8YkmeFlm6nHNmYviT4iZ4e3TfBx";
 
-  $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
-    console.log(response);
-  });
-  */
-  // creating buttons and connecting it to ajax
-
-  // Generic function for capturing the movie name from the data-attribute
-  function alertEmotionName() {
+  function createEmotionGifs() {
     $("button").on("click", function() {
       let x = $(this).data("search");
 
@@ -46,27 +36,17 @@ $(document).ready(function() {
         }
       });
     });
-    // YOUR CODE GOES HERE!!!
   }
 
-  // Function for displaying movie data
+  // display the buttons for emotions
   function renderButtons() {
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise we will have repeat buttons)
     $("#buttons-view").empty();
-
-    // Looping through the array of movies
+    // looping through the array of emotions
     for (var i = 0; i < emotions.length; i++) {
-      // Then dynamicaly generating buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
       var a = $("<button>");
-      // Adding a class
       a.addClass("emotion");
-      // Added a data-attribute
       a.attr("data-search", emotions[i]);
-      // Provided the initial button text
       a.text(emotions[i]);
-      // Added the button to the HTML
       $("#buttons-view").append(a);
     }
   }
@@ -75,25 +55,15 @@ $(document).ready(function() {
   $("#add-emotion").on("click", function(event) {
     event.preventDefault();
 
-    // This line grabs the input from the textbox
     var movie = $("#emotion-input")
       .val()
       .trim();
 
-    // The movie from the textbox is then added to our array
     emotions.push(emotion);
 
-    // Calling renderButtons which handles the processing of our movie array
     renderButtons();
   });
 
-  // Function for displaying the movie info
-  // We're adding a click event listener to all elements with the class "movie"
-  // We're adding the event listener to the document itself because it will
-  // work for dynamically generated elements
-  // $(".movies").on("click") will only add listeners to elements that are on the page at that time
-  $(document).on("click", ".emotion", alertEmotionName);
-
-  // Calling the renderButtons function to display the intial buttons
+  $(document).on("click", ".emotion", createEmotionGifs);
   renderButtons();
 });
